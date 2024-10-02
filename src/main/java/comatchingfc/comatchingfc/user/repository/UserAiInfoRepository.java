9b5fc6e020c89ffcc616398df62d5cb947b1628e
popeye0618 <popeye0618@gmail.com>
2024-10-02 23:16:52 +0900

    fix: 필드 명 변경

--- src/main/java/comatchingfc/comatchingfc/user/repository/UserAiInfoRepository.java
+++ src/main/java/comatchingfc/comatchingfc/user/repository/UserAiInfoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserAiFeatureRepository extends JpaRepository<UserAiInfo, Long> {
+public interface UserAiInfoRepository extends JpaRepository<UserAiInfo, Long> {
 }
