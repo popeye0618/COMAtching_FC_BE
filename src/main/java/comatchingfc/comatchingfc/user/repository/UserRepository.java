36de3d7af17d153dc77c4b0043651afa6c3c7338
popeye0618 <popeye0618@gmail.com>
2024-10-02 23:17:08 +0900

    feat: 필드 추가

--- src/main/java/comatchingfc/comatchingfc/user/repository/UserRepository.java
+++ src/main/java/comatchingfc/comatchingfc/user/repository/UserRepository.java
@@ -4,6 +4,10 @@ import comatchingfc.comatchingfc.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
+
+    Optional<Users> findByIdentifyKey(String identifyKey);
 }
