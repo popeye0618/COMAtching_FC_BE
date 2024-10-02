fff9e123f9d462e8225ece6f37cd36a8229f0b0b
popeye0618 <popeye0618@gmail.com>
2024-10-02 23:16:30 +0900

    feat: JWT 에러코드 추가

--- src/main/java/comatchingfc/comatchingfc/utils/response/ResponseCode.java
+++ src/main/java/comatchingfc/comatchingfc/utils/response/ResponseCode.java
@@ -9,7 +9,11 @@ public enum ResponseCode {
 	//General response
 	SUCCESS(200, "GEN-000", HttpStatus.OK, "Success"),
 	BAD_REQUEST(400, "GEN-001", HttpStatus.BAD_REQUEST, "Bad Request"),
-	INTERNAL_SERVER_ERROR(500, "GEN-002", HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred");
+	INTERNAL_SERVER_ERROR(500, "GEN-002", HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred"),
+
+	//Auth response
+	TOKEN_EXPIRED(401, "SEC-001", HttpStatus.UNAUTHORIZED, "Token is expired"),
+	JWT_ERROR(403, "SEC-002", HttpStatus.FORBIDDEN, "Jwt error occurred");
 
 	private final Integer status;
 	private final String code;
