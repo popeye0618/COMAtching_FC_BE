256469f8660cbd517d52ac84129a6e0f6a28844b
popeye0618 <popeye0618@gmail.com>
2024-10-02 12:26:15 +0900

    build: 프로젝트 초기 설정

---
+++ src/main/java/comatchingfc/comatchingfc/ComatchingFcApplication.java
@@ -0,0 +1,13 @@
+package comatchingfc.comatchingfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComatchingFcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComatchingFcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/comatchingfc/comatchingfc/ComatchingFcApplicationTests.java
@@ -0,0 +1,13 @@
+package comatchingfc.comatchingfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComatchingFcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
