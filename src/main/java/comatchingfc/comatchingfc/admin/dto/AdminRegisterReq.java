01e368bac96b02530b1a299f64e94a7b2fee6c32
popeye0618 <popeye0618@gmail.com>
2024-10-04 19:45:35 +0900

    feat : DTO 값 검증 추가

--- src/main/java/comatchingfc/comatchingfc/admin/dto/AdminRegisterReq.java
+++ src/main/java/comatchingfc/comatchingfc/admin/dto/AdminRegisterReq.java
@@ -1,12 +1,23 @@
 package comatchingfc.comatchingfc.admin.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AdminRegisterReq {
+
+    @NotNull
+    @NotEmpty
     private String username;
+
+    @NotNull
+    @NotEmpty
     private String accountId;
+
+    @NotNull
+    @NotEmpty
     private String password;
 }
