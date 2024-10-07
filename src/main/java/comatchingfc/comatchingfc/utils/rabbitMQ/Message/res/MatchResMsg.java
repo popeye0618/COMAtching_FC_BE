996db7018d7215a381988fd4237aeb49540407c2
greensnapback0229 <smdmim@gmail.com>
2024-10-08 01:30:22 +0900

    feat: add message dto about match

---
+++ src/main/java/comatchingfc/comatchingfc/utils/rabbitMQ/Message/res/MatchResMsg.java
@@ -0,0 +1,9 @@
+package comatchingfc.comatchingfc.utils.rabbitMQ.Message.res;
+
+import lombok.Getter;
+
+	@Getter
+	public class MatchResMsg {
+		private String stateCode;
+		private String enemyUuid;
+	}
