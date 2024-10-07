5422f0000eae708a9e8a330dd35a9df2570e9fff
greensnapback0229 <smdmim@gmail.com>
2024-10-07 23:15:35 +0900

    feat: add dto about reserve number authentication

---
+++ src/main/java/comatchingfc/comatchingfc/utils/rabbitMQ/Message/req/ReserveAuthReq.java
@@ -0,0 +1,12 @@
+package comatchingfc.comatchingfc.utils.rabbitMQ.Message.req;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReserveAuthReq {
+	private String reserveNumber;
+}
