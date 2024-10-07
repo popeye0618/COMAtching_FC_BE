32f5726727ad809d9754257af1ca7312d23eaca5
greensnapback0229 <smdmim@gmail.com>
2024-10-08 01:31:53 +0900

    chore: 매칭 서비스 rabbitMQ Util 추가

---
+++ src/main/java/comatchingfc/comatchingfc/utils/rabbitMQ/MatchingRabbitMQUtil.java
@@ -0,0 +1,56 @@
+package comatchingfc.comatchingfc.utils.rabbitMQ;
+
+import java.util.UUID;
+
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+
+import comatchingfc.comatchingfc.exception.BusinessException;
+import comatchingfc.comatchingfc.user.entity.CheerPropensity;
+import comatchingfc.comatchingfc.utils.rabbitMQ.Message.req.MatchReqMsg;
+import comatchingfc.comatchingfc.utils.rabbitMQ.Message.res.MatchResMsg;
+import comatchingfc.comatchingfc.utils.response.ResponseCode;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class MatchingRabbitMQUtil {
+	private final RabbitTemplate rabbitTemplate;
+
+	@Value("${rabbitmq.routing-keys.match-request}")
+	private String matchRequestQueue;
+
+	public MatchingRabbitMQUtil(RabbitTemplate rabbitTemplate){
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
+	/**
+	 * rabbitMQ 매칭 요청
+	 * @param requestMsg : 매칭 요청 Dto
+	 * @return 매칭 결과
+	 */
+	public MatchResMsg requestMatch(MatchReqMsg requestMsg){
+		String requestId = UUID.randomUUID().toString();
+		CorrelationData correlationData = new CorrelationData(requestId);
+		ParameterizedTypeReference<MatchResMsg> responseType = new ParameterizedTypeReference<MatchResMsg>(){};
+
+		log.info("[MatchingRabbitMQUtil requestMatch] request={}", requestMsg.toJsonString());
+		MatchResMsg responseMsg =  rabbitTemplate.convertSendAndReceiveAsType(
+			matchRequestQueue,
+			requestMsg,
+			(MessagePostProcessor) null,
+			correlationData,
+			responseType);
+
+		if(responseMsg == null){
+			throw new BusinessException(ResponseCode.GEN_MATCH_FAIL);
+		}
+
+		log.info("[MatchingRabbitMQUtil requestMatch] stateCode = {} / responseMsg.getEnemyUuid={} ", responseMsg.getStateCode(), responseMsg.getEnemyUuid());
+		return responseMsg;
+	}
+}
