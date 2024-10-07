8c64121938a45915ba26c43dd88779bcba8e5b94
greensnapback0229 <smdmim@gmail.com>
2024-10-07 23:16:07 +0900

    feat: add rabbitMQ Util for reserve number authentication

---
+++ src/main/java/comatchingfc/comatchingfc/utils/rabbitMQ/AuthRabbitMQUtil.java
@@ -0,0 +1,47 @@
+package comatchingfc.comatchingfc.utils.rabbitMQ;
+
+import java.util.UUID;
+
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+
+import comatchingfc.comatchingfc.utils.rabbitMQ.Message.req.ReserveAuthReq;
+import comatchingfc.comatchingfc.utils.rabbitMQ.Message.res.ReserveAuthRes;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class AuthRabbitMQUtil {
+
+	private final RabbitTemplate rabbitTemplate;
+
+	@Value("${rabbitmq.routing-keys.reserve-auth}")
+	private String matchRequestQueue;
+
+	public AuthRabbitMQUtil(RabbitTemplate rabbitTemplate){
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
+
+	public boolean checkReserveNumber(String reserveNumber){
+		String requestId = UUID.randomUUID().toString();
+
+		ReserveAuthReq requestMsg = new ReserveAuthReq(reserveNumber);
+		CorrelationData correlationData = new CorrelationData(requestId);
+		ParameterizedTypeReference<ReserveAuthRes> responseType = new ParameterizedTypeReference<ReserveAuthRes>(){};
+
+
+		ReserveAuthRes responseMsg =  rabbitTemplate.convertSendAndReceiveAsType(
+			matchRequestQueue,
+			requestMsg,
+			(MessagePostProcessor) null,
+			correlationData,
+			responseType);
+
+		return responseMsg.isAuthSuccess();
+	}
+}
