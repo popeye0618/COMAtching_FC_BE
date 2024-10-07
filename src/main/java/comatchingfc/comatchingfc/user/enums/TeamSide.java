7b14c7140cd936f90471edb17a96163a2e579fea
greensnapback0229 <smdmim@gmail.com>
2024-10-07 23:14:08 +0900

    feat: add teamSide and field for entity

---
+++ src/main/java/comatchingfc/comatchingfc/user/enums/TeamSide.java
@@ -0,0 +1,13 @@
+package comatchingfc.comatchingfc.user.enums;
+
+public enum TeamSide {
+	HOME("HOME"),
+	AWAY("AWAY");
+
+	private final String value;
+
+	TeamSide(String value){
+		this.value = value;
+	}
+
+}
