76b77f9d90cfe13d278aeaac92e99052b75fc195
greensnapback0229 <smdmim@gmail.com>
2024-10-10 05:24:46 +0900

    feat: 매칭 서비스 DTO 생성

--- src/main/java/comatchingfc/comatchingfc/match/dto/req/MatchReq.java
+++ src/main/java/comatchingfc/comatchingfc/match/dto/req/MatchReq.java
@@ -2,9 +2,11 @@ package comatchingfc.comatchingfc.match.dto.req;
 
 import comatchingfc.comatchingfc.user.enums.Gender;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class MatchReq {
-	private String uuid;
+
 	private Gender genderOption;
 }
