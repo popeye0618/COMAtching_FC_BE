81f9f852f68d96cb2c34fd37b47b8756b380438f
popeye0618 <popeye0618@gmail.com>
2024-10-10 22:36:29 +0900

    fix : 유저 로그인 응답 dto 수정

--- src/main/java/comatchingfc/comatchingfc/auth/dto/Res/UserLoginRes.java
+++ src/main/java/comatchingfc/comatchingfc/auth/dto/Res/UserLoginRes.java
@@ -6,12 +6,12 @@ import comatchingfc.comatchingfc.auth.jwt.dto.TokenRes;
 import comatchingfc.comatchingfc.user.enums.TeamSide;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 public class UserLoginRes {
 	private TeamSide teamSide;
+	private String role;
 
 	@JsonIgnore
 	private TokenRes tokenRes;
