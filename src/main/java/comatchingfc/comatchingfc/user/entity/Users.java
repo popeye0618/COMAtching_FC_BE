c0d314de303bf39bd1b2d7f2982108bd0a880bc3
popeye0618 <popeye0618@gmail.com>
2024-10-14 13:21:25 +0900

    fix: 편의 메소드 수정

--- src/main/java/comatchingfc/comatchingfc/user/entity/Users.java
+++ src/main/java/comatchingfc/comatchingfc/user/entity/Users.java
@@ -85,13 +85,5 @@ public class Users extends BaseEntity {
         this.deactivated = false;
     }
 
-    public void addMatchingHistory(MatchingHistory matchingHistory) {
-        this.matchingHistory = matchingHistory;
-        if (matchingHistory.getApplier() != null && matchingHistory.getApplier() != this) {
-            matchingHistory.getApplier().addMatchingHistory(matchingHistory);
-        }
-        if (matchingHistory.getEnemy() != null && matchingHistory.getEnemy() != this) {
-            matchingHistory.getEnemy().addMatchingHistory(matchingHistory);
-        }
-    }
+    public void addMatchingHistory(MatchingHistory matchingHistory){ this.matchingHistory = matchingHistory; }
 }
